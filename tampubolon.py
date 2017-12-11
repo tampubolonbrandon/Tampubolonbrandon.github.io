58ac92361dae2779842e4fa918434674ab98e389
brandon tampubolon <brandontam.81100@redlandsschools.net>
2017-12-11 16:58:44 +0000

    download

--- tampubolon.py
+++ tampubolon.py
@@ -2,7 +2,7 @@ x = int(input("Enter a number: "))
 print(2*x)
 if (x % 2) == x:
 
-   print("{x} is Even")
+   if x % 2 == 0:
+    print (" is odd")
 else:
-   print("your number is Odd")
-
+    print (" is even")
